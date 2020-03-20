cea5eaf958d6fd8748ea8f109dfe9ca8f7c35302
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-03-20 16:00:56 +0200

    fix: Material checkbox group: theme variant VERTICAL doesn't work #158

--- test/test-suites.js
+++ test/test-suites.js
@@ -1,5 +1,6 @@
 window.VaadinCheckboxSuites = [
   'vaadin-checkbox_test.html',
   'vaadin-checkbox-group_test.html',
-  'accessibility.html'
+  'accessibility.html',
+  'vaadin-checkbox-group-material_test.html'
 ];
