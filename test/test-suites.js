7cc5f64298615d21342d9e2767159e36958f6e34
hongduc-phan <phduc.phanhongduc@gmail.com>
2020-03-23 12:14:26 +0200

    add visual tests for theme variant vertical

--- test/test-suites.js
+++ test/test-suites.js
@@ -1,6 +1,5 @@
 window.VaadinCheckboxSuites = [
   'vaadin-checkbox_test.html',
   'vaadin-checkbox-group_test.html',
-  'accessibility.html',
-  'vaadin-checkbox-group-material_test.html'
+  'accessibility.html'
 ];
